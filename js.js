07b4ffc1cd9d3de844ad31c1c3f5c65085c523a4
Adrian Llerena <a.llerena001@gmail.com>
2018-02-19 20:09:02 -0500

    prototypes

--- js.js
+++ js.js
@@ -5,5 +5,24 @@ function Item (name, price){
 
 var myItem = new Item("orange", "39c");
 
-console.log(myItem.name);
-console.log(myItem);
+// console.log(myItem.name);
+// console.log(myItem);
+
+
+//Sharing behaviors through prototypes
+
+function Item(name, price){
+  this.name = name;
+  this.price = price;
+}
+
+Item.prototype.calculatePrice = function() {
+  console.log ("the price is " + (this.price - 0.05));
+};
+
+var ball = new Item('soccer ball', 15);
+ball.calculatePrice();
+
+
+var fruit = new Item('fruit', 10);
+fruit.calculatePrice();
